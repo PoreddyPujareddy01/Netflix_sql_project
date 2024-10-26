291352c3edf285d6637ed81b7e5fcb4d10ad93a0
Poreddy Puja Reddy <pujareddyporeddy@gmail.com>
2024-10-26 16:41:10 +0530

    Create Netflix_project_questions.sql

---
+++ Netflix_project_questions.sql
@@ -0,0 +1,15 @@
+---- 10 Questions and Solutions
+
+EASY
+1. What is the count of each show type (Movie, TV Show)?
+2. What are the top 5 most common ratings on Netflix?
+3. How many unique countries are represented in the Netflix dataset?
+INTERMEDIATE
+4. What are the top 5 most common release years for shows and movies on Netflix?
+5. Who are the top 5 directors with the most titles?
+6. What is the average release year by show type?
+HARD
+7. What are the most common genre combinations on Netflix?
+8. How many shows are there per duration category (e.g., Movies by Minutes, TV Shows by Seasons)?
+9. Which shows were released in the latest year?
+10. What are the top 5 countries with the most titles on Netflix?
